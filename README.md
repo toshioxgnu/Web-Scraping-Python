bcc27eddeb8387b529271bc25859a5de5c64f535
Jose Gonzalez Riquelme <jgonzalezr5@outlook.es>
2019-11-10 17:26:13 -0300

    Finish

---
+++ README.md
@@ -0,0 +1,3 @@
+Aprendiendo a hacer scraping a las paginas web con python. 
+
+Pagina web Objetivo: [link to Objective Page] (flipkart.com/laptops/~buyback-guarantee-on-laptops-/pr?sid=6bo%2Cb5g&uniqBStoreParam1=val1&wid=11.productCard.PMU_V2)
--- web-s.py
+++ web-s.py
@@ -1,17 +1,25 @@
 from selenium import webdriver 
-from BeautifulSoup import BeautifulSoup
-import panda as pd 
+from bs4 import BeautifulSoup
+import pandas as pd 
 
-driver = webdriver.Chrome("/usr/lib/chromium-browser/chromiumdriver")
+driver = webdriver.Chrome("/usr/bin/chromedriver")
 
 products = []
 prices = []
 ratings = []
-driver.get("<a href="https://www.flipkart.com/laptops/">https://www.flipkart.com/laptops/</a>~buyback-guarantee-on-laptops-/pr?sid=6bo%2Cb5g&uniq")
+driver.get('https://www.flipkart.com/laptops/~buyback-guarantee-on-laptops-/pr?sid=6bo%2Cb5g&uniqBStoreParam1=val1&wid=11.productCard.PMU_V2')
 
 
 content = driver.page_source 
-soup= BeautifulSoup(content)
-for a in soup.findAll('a',href=True, attrs={'class':'_3wU53n'}):
-	name=a.find('div', attrs={'class':'_3wU53n'})
-	
+soup = BeautifulSoup(content, features="html.parser")
+for a in soup.findAll('a',href=True, attrs={'class':'_31qSD5'}):
+	name = a.find('div', attrs={'class':'_3wU53n'})
+	price = a.find('div', attrs={'class':'_6BWGkk'})
+	rating = a.find('div', attrs={'class':'niH0FQ'})
+	products.append(name.text)
+	prices.append(price.text)
+	ratings.append(rating.text)
+
+
+df =  pd.DataFrame({'Product Name':products,'Price':prices,'Rating':ratings})
+df.to_csv('products.csv', index=False, encoding='utf-8')
